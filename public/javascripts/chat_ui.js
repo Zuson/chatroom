2c046a03e9221bc321556c2dc18b4badc590280b
Zuson <1244923939@qq.com>
2019-01-14 19:18:23 +0800

    修复socket版本的问题

--- public/javascripts/chat_ui.js
+++ public/javascripts/chat_ui.js
@@ -24,6 +24,8 @@ function processUserInput (chatApp, socket) {
 var socket = io.connect();
 $(document).ready(function () {
 	var chatApp = new Chat(socket);
+
+	// 获取初始用户名
 	socket.on('nameResult', function(result) {
 		var message;
 		if(result.success) {
@@ -33,14 +35,20 @@ $(document).ready(function () {
 		}
 		$('#messages').append(divSystemContentElement(message));
 	});
+
+	// 侦听
 	socket.on('joinResult', function(result) {
 		$('#room').text(result.room);
 		$('#message').append(divSystemContentElement('Room changed'));
 	});
+
+	// 侦听广播信息
 	socket.on('message', function(message) {
 		var newElement = $('<div></div>').text(message.text);
 		$('#messages').append(newElement);
 	});
+
+	// 侦听房间列表信息
 	socket.on('rooms', function(rooms) {
 		$('#room-list').empty();
 		for(var room in rooms){
@@ -53,9 +61,11 @@ $(document).ready(function () {
 			$('#send-message').focus();
 		});
 	});
+
 	setInterval(function() {
 		socket.emit('rooms');
 	}, 1000);
+
 	$('#send-message').focus();
 	$('#send-form').submit(function() {
 		processUserInput(chatApp, socket);
